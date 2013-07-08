1b67012af59af20c04910c7ac931e605ae228bb7
Josh Renner <mail@joshrenner.com>
2013-07-08 16:52:16 -0500

    Adding Grunt

---
+++ web/downloadSuite.js
@@ -0,0 +1,9 @@
+new cssTest.model.testing.suite()
+    .name("download")
+    .test("h1", {
+        color:"#000",
+        display:"block"
+    }, "h1")
+    .test("body", {
+        background:"red"
+    }, "body")
